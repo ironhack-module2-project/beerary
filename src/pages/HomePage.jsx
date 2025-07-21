fb075ebe71245b34ea6bdbeae4b559013f058deb
Sitripi0 <216026207+Sitripi0@users.noreply.github.com>
2025-07-21 12:57:30 +0200

    Implement basic design, limit image sizes and more-information collapse

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,11 +24,37 @@ function HomePage() {
       <div className="flex justify-center">
         <div className="card w-96 bg-base-100 card-xs shadow-sm card-side">
           <figure>
-            <img src={`${BASE_URL}/images/${randomBeer.image}`} alt="beer" />
+            <img
+              className="max-w-[200px] h-[394px]"
+              src={`${BASE_URL}/images/${randomBeer.image}`}
+              alt="beer"
+            />
           </figure>
-          <div className="card-body">
-            <h2 className="card-title">Name:{randomBeer.name}</h2>
-            <p>Tagline: {randomBeer.tagline}</p>
+          {/* Eliminé la altura fija h-[394px] para que el contenido crezca */}
+          <div className="card-body max-w-[200px] text-left">
+            <h2 className="card-title text-xl">{randomBeer.name}</h2>
+            <p className="max-h-[20px] italic text-base">{randomBeer.tagline}</p>
+            <br />
+            <p>{randomBeer.description}</p>
+            <br />
+
+            <div
+              tabIndex={0}
+              className="collapse bg-base-100 border border-base-300 max-w-[394px]"
+            >
+              <div className="collapse-title font-semibold">More Information</div>
+              <div className="collapse-content text-left">
+                <p><strong>Enjoy it more with:</strong></p>
+                <ul className="list-disc list-inside">
+                  {randomBeer.food_pairing?.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+                <br />
+                <p><strong>Brewer Tips:</strong> {randomBeer.brewer_tips}</p>
+              </div>
+            </div>
+
             <div className="justify-end card-actions">
               <button className="btn btn-primary">Add Beer</button>
             </div>
@@ -38,4 +64,5 @@ function HomePage() {
     </main>
   );
 }
+
 export default HomePage;
