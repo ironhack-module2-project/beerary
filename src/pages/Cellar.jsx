89ce911139a619bfbd830edeeb6b40170130cc38
Sitripi0 <216026207+Sitripi0@users.noreply.github.com>
2025-07-22 12:56:01 +0200

    feat CRUD cellar

--- src/pages/Cellar.jsx
+++ src/pages/Cellar.jsx
@@ -6,6 +6,39 @@ const BASE_URL = "https://punkapi.online/v3";
 
 function Cellar() {
   const [cellar, setCellar] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  const handleDelete = (id) => {
+
+    axios
+      .delete(`${API_URL}/beers/${id}.json`)
+      .then(() => {
+        console.log("succes on Delete")
+        axios
+          .get(`${API_URL}/beers.json`)
+          .then((response) => {
+            const data = response.data;
+            if (data) {
+              const formatted = Object.entries(data).map(([key, value]) => ({
+                firebaseId: key,
+                ...value,
+              }));
+              console.log(formatted)
+              setCellar(formatted);
+              setLoading(false)
+            } else {
+              setCellar([])
+              setLoading(false)
+            }
+          })
+          .catch((error) =>
+            console.log("Error getting data from API_URL", error)
+          );
+      })
+      .catch((error) => console.log("ERROR on DELETE element", error))
+
+  }
 
   useEffect(() => {
     axios
@@ -14,10 +47,15 @@ function Cellar() {
         const data = response.data;
         if (data) {
           const formatted = Object.entries(data).map(([key, value]) => ({
-            id: key,
+            firebaseId: key,
             ...value,
           }));
+          console.log(formatted)
           setCellar(formatted);
+          setLoading(false)
+        } else {
+          setCellar([])
+          setLoading(false)
         }
       })
       .catch((error) =>
@@ -25,13 +63,19 @@ function Cellar() {
       );
   }, []);
 
+   if (loading)
+     return (
+       <span className="loading loading-spinner text-primary loading-xl"></span>
+
+     );
+
+
   return (
     <div className="flex justify-center">
       <div className="carousel carousel-vertical rounded-box h-[32rem] overflow-y-auto w-full max-w-md p-2">
-        {cellar.length === 0 ? (
-          <p className="text-white p-4">NO BEERS STORED</p>
-        ) : (
-          cellar.map((beer) => (
+        
+        {cellar.length === 0 && <p className="text-white p-4">NO BEERS STORED</p>}
+        {cellar && cellar.map((beer) => (
             <div key={beer.id} className="carousel-item mb-4 ">
               <div className="glass flex flex-col sm:flex-row items-center sm:items-start gap-4 p-4 bg-base-200 rounded-lg w-full shadow-md">
                 <img
@@ -45,12 +89,13 @@ function Cellar() {
                   <button className="btn btn-primary btn-sm mt-2 self-center sm:self-start">
                     Show More
                   </button>
-                </div>                         
+                  <button className="btn btn-secondary btn-sm mt-2 self-center sm:self-start" onClick={() => handleDelete(beer.firebaseId)}>Delete</button>
+                </div>
               </div>
-              
+
             </div>
           ))
-        )}
+        }
       </div>
     </div>
   );
