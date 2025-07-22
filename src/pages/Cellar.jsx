d982c0f59839a93df2c2b028fd684c60cba9faf7
Sitripi0 <216026207+Sitripi0@users.noreply.github.com>
2025-07-22 14:54:02 +0200

    Implement Modal Cellar

--- src/pages/Cellar.jsx
+++ src/pages/Cellar.jsx
@@ -7,6 +7,7 @@ const BASE_URL = "https://punkapi.online/v3";
 function Cellar() {
   const [cellar, setCellar] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [modalBeer, setModalBeer] = useState({})
 
 
   const handleDelete = (id) => {
@@ -63,40 +64,75 @@ function Cellar() {
       );
   }, []);
 
-   if (loading)
-     return (
-       <span className="loading loading-spinner text-primary loading-xl"></span>
+  const openModal = (beer) => {
+    setModalBeer(beer);
+    document.getElementById("my_modal_3").showModal();
+  };
 
-     );
+  if (loading)
+    return (
+      <span className="loading loading-spinner text-primary loading-xl"></span>
+
+    );
+
+
 
 
   return (
     <div className="flex justify-center">
       <div className="carousel carousel-vertical rounded-box h-[32rem] overflow-y-auto w-full max-w-md p-2">
-        
+
         {cellar.length === 0 && <p className="text-white p-4">NO BEERS STORED</p>}
         {cellar && cellar.map((beer) => (
-            <div key={beer.id} className="carousel-item mb-4 ">
-              <div className="glass flex flex-col sm:flex-row items-center sm:items-start gap-4 p-4 bg-base-200 rounded-lg w-full shadow-md">
-                <img
-                  src={`${BASE_URL}/images/${beer.image}`}
-                  alt={beer.name}
-                  className="w-24 h-24 object-contain rounded"
-                />
-                <div className="flex flex-col text-center sm:text-left">
-                  <h2 className="text-lg font-bold">{beer.name}</h2>
-                  <p className="italic text-sm text-gray-400">{beer.tagline}</p>
-                  <button className="btn btn-primary btn-sm mt-2 self-center sm:self-start">
-                    Show More
-                  </button>
-                  <button className="btn btn-secondary btn-sm mt-2 self-center sm:self-start" onClick={() => handleDelete(beer.firebaseId)}>Delete</button>
-                </div>
+          <div key={beer.id} className="carousel-item mb-4 ">
+            <div className="glass flex flex-col sm:flex-row items-center sm:items-start gap-4 p-4 bg-base-200 rounded-lg w-full shadow-md">
+              <img
+                src={`${BASE_URL}/images/${beer.image}`}
+                alt={beer.name}
+                className="w-24 h-24 object-contain rounded"
+              />
+              <div className="flex flex-col text-center sm:text-left">
+                <h2 className="text-lg font-bold">{beer.name}</h2>
+                <p className="italic text-sm text-gray-400">{beer.tagline}</p>
+                <button className="btn btn-primary btn-sm mt-2 self-center sm:self-start" onClick={() => openModal(beer)}>
+                  Show More
+                </button>
+                <button className="btn btn-secondary btn-sm mt-2 self-center sm:self-start" onClick={() => handleDelete(beer.firebaseId)}>Delete</button>
               </div>
-
             </div>
-          ))
-        }
+
+          </div>
+        ))
+        }        
       </div>
+
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">{modalBeer.name}</h3>
+          <figure className="text-center">
+            <img
+              className="max-w-[200px] h-[394px]"
+              src={`${BASE_URL}/images/${modalBeer.image}`}
+              alt="beer"
+            />
+          </figure>
+          <div className="card-body text-left">
+            <h2 className="card-title text-xl">{modalBeer.name}</h2>
+            <p className="max-h-[20px] italic text-base">{modalBeer.tagline}</p>
+            <br />
+            <p>{modalBeer.description}</p>
+            <br />
+          </div>
+          <p className="py-4">Press ESC key or click on ✕ button to close</p>
+        </div>
+      </dialog>
+
     </div>
   );
 }
